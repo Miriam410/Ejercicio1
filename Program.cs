2b4e5bb8c77a4e7a6ab72e89e4901b3bc9d403b9
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-03-20 17:24:55 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+   class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo.");
+            Console.WriteLine("Por favor, ingrese una tecla para salir.");
+            Console.Read();
+        }
+    }
+}
